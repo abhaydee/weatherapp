4a0bfcc1aa58f073330843a04a59f1750e58aee7
abhaydee <abhaygirish1997@gmail.com>
2019-07-24 15:20:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Weather from './Components/Weather';
+function App() {
+  return (
+    <div className="App">
+        <Weather></Weather>
+    </div>
+  );
+}
+
+export default App;
