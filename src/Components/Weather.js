670c9ef34600f635ee16f5fc89ffee2fea677fb8
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-10-04 23:04:40 +0530

    :wrench: refactor: destructured state, fixed "city not found" bug

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,53 +1,43 @@
 import React, { Component } from "react";
-import { Button, Nav, NavDropdown, Navbar, NavbarBrand } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 class Weather extends Component {
   state = {
     query: "",
-    WeatherData: "",
+    displaydata: undefined,
     show: false
   };
 
   updatequery = query => {
-    this.setState({ query: query.trim() });
+    console.log(query)
     if (query === "") {
       this.setState({ show: false });
-    }
+    }       
+    this.setState({ query: query.trim() });
   };
 
-  clearquery = query => {
-    this.setState({ query: "" });
-  };
-  display = () => {
-    if (this.state.query === "") {
-      this.setState({ show: false });
-    }
-  };
+  clearquery = () => this.setState({query: '', displaydata: undefined, show: false});
+
   displayWeather = () => {
     fetch(`
 https://api.openweathermap.org/data/2.5/weather?q=${this.state.query}&appid=55198d4f87f289ec883df973bd75e1a3
         `)
       .then(Response => {
-        Response.json().then(json => {
-          console.log(json);
+        Response.json().then(displaydata => {
           this.setState({
-            WeatherData: json,
+            displaydata,
             show: true
           });
-          console.log(this.state.WeatherData);
         });
       })
-      .catch(reject => {
+      .catch(displaydata => {
         //console.log("no  such data");
-        this.setState({ WeatherData: reject, show: true });
-
-        console.log(this.state.WeatherData);
+        this.setState({ displaydata, show: true });
       });
   };
 
   render() {
-    const temp = null;
-    let displaydata = this.state.WeatherData;
+    let {displaydata, show, query} = this.state;
     return (
       <div>
         <div className="listing">
@@ -56,38 +46,36 @@ https://api.openweathermap.org/data/2.5/weather?q=${this.state.query}&appid=5519
             className="search-weather"
             type="text"
             placeholder="list-weather"
-            value={this.state.query}
+            value={query}
             onChange={event => this.updatequery(event.target.value)}
-            onClick={this.display}
-          ></input>
-          {console.log(this.state.query)}
-          <Button onClick={this.displayWeather}>CLICK_ME</Button>
+          />
 
-          <Button onClick={this.clearquery}>CLEAR</Button>
+          <Button onClick={() => this.displayWeather()}>CLICK_ME</Button>
+          <Button variant="danger" onClick={() => this.clearquery()}>CLEAR</Button>
         </div>
-        {console.log(displaydata.name)}
-        {this.state.show &&
+        {console.log('', displaydata)}
+        {show &&
           displaydata &&
-          displaydata.cod == "200" &&
-          this.state.query != "" && (
+          displaydata.cod === 200 &&
+          query !== "" && (
             <div className="displaying">
               <h1>
-                The Temperature of the city is
-                {Math.round(displaydata.main.temp - 273.15)}
+                The Temperature of the city is&nbsp;
+                {Math.round(displaydata.main.temp - 273.15)} C
               </h1>
               <h1>
-                The Max Temperature of the city is
-                {Math.round(displaydata.main.temp_max - 273.15)}
+                The Max Temperature of the city is&nbsp;
+                {Math.round(displaydata.main.temp_max - 273.15)} C
               </h1>
               <h1>
-                The minimum Temperature of the city is
-                {Math.round(displaydata.main.temp_min - 273.15)}
+                The minimum Temperature of the city is&nbsp;
+                {Math.round(displaydata.main.temp_min - 273.15)} C
               </h1>
             </div>
           )}
-        {this.state.show && displaydata.cod == "404" && (
+        {displaydata && !displaydata.coord && (
           <div className="displaying">
-            <h1>City no found</h1>
+            <h1>City not found</h1>
           </div>
         )}
       </div>
